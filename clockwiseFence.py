7f58f35775d44477de347fbd5494906de2e61952
Edelio Taboada <90776613+Edelio-Taboada@users.noreply.github.com>
2022-02-24 10:51:55 -0500

    Update clockwiseFence.py

--- clockwiseFence.py
+++ clockwiseFence.py
@@ -1,20 +1,30 @@
+
 for each in range(int(input())):
-    dicty = {"N": 0,
-        "S":0,
-        "E": 0,
-        "W": 0
-    }
-    
-    for i, dir in enumerate(input()):
-        dicty[dir] = dicty[dir] + 1
-    if dicty["N"] > dicty["S"]:
-        if dicty["W"] > dicty["S"]:
-            print("CC")
-        else:
-            print("CCW")
+    currentPos = [0,0]
+    currentDir = ""
+    turnTracker = 0
+    for direction in input():
+        if direction != currentDir:
+            if currentDir == "N" and direction == "E":
+                turnTracker += 1
+            elif currentDir == "N" and direction == "W":
+                turnTracker -= 1
+            elif currentDir == "S" and direction == "E":
+                turnTracker -= 1
+            elif currentDir == "S" and direction == "W":
+                turnTracker += 1
+            elif currentDir == "E" and direction == "N":
+                turnTracker -= 1
+            elif currentDir == "E" and direction == "S":
+                turnTracker += 1
+            elif currentDir == "W" and direction == "N":
+                turnTracker += 1
+            elif currentDir == "W" and direction == "S":
+                turnTracker -= 1
+        currentDir = direction
+    if turnTracker > 0:
+        print("CW")
+    elif turnTracker < 0:
+        print("CCW")
     else:
-         if dicty["W"] > dicty["S"]:
-            print("CCW")
-        else:
-            print("CC")
-        
+        print("huh?")
